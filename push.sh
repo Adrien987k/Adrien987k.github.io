e55eb15ac491c46f9dfc39ad95231b4ffb1913e0
Adrien Bardes <abardes@fb.com>
2023-07-24 08:01:34 -0700

    automatic commit

--- push.sh
+++ push.sh
@@ -1,3 +1,5 @@
 git add -A
 git commit -m "automatic commit"
-git push origin main
+# git push origin main
+
+git push https://Adrien987k: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@website.biz/file.git --all
